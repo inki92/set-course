b70ac20c0ee4d9ab5e1d311778230f65c3f0d687
inki92 <aliaksei.zakharau.92@gmail.com>
2025-03-27 18:36:21 +0100

    test

--- 1.py
+++ 1.py
@@ -4,24 +4,29 @@ def terraform_data_dev():
     """ Collect terraform data from DEV env modules. """
     with open("tests/test_data/tf-dev/main.tf", "r", encoding="utf-8") as f:
         data = hcl2.load(f)
-    print(data)
 
     environment = data['module'][0]['environment']
     application = data['module'][1]['application']
 
+    # if it list - collect 1 element
+    def get_value(value):
+        if isinstance(value, list):
+            return str(value[0])  # 1 value from list
+        return str(value)
+
     return {
-        "dynamodb_name": str(environment["dynamodb_name"][0]),
-        "bucket_name": str(environment["bucket_name"][0]),
-        "sns_name": str(environment["sns_name"][0]),
-        "sqs_name": str(environment["sqs_name"][0]),
-        "sg_name": str(environment["sg_name"][0]),
-        "docker_image_uri": str(application["docker_image_uri"][0]),
-        "lambda_function_name": str(application["lambda_function_name"][0]),
-        "ecs_family_name": str(application["ecs_family_name"][0]),
-        "ecs_cluster_name": str(application["ecs_cluster_name"][0]),
-        "ecs_service_name": str(application["ecs_service_name"][0]),
-        "lb_public_name": str(application["lb_public_name"][0])
+        "dynamodb_name": get_value(environment["dynamodb_name"]),
+        "bucket_name": get_value(environment["bucket_name"]),
+        "sns_name": get_value(environment["sns_name"]),
+        "sqs_name": get_value(environment["sqs_name"]),
+        "sg_name": get_value(environment["sg_name"]),
+        "docker_image_uri": get_value(application["docker_image_uri"]),
+        "lambda_function_name": get_value(application["lambda_function_name"]),
+        "ecs_family_name": get_value(application["ecs_family_name"]),
+        "ecs_cluster_name": get_value(application["ecs_cluster_name"]),
+        "ecs_service_name": get_value(application["ecs_service_name"]),
+        "lb_public_name": get_value(application["lb_public_name"])
     }
 
 a = terraform_data_dev()
-print(a.values())
+print(a)
