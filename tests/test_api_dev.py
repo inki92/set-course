10c05a165666b7ee48579c2d9041c438cfee83fb
inki92 <aliaksei.zakharau.92@gmail.com>
2025-03-28 17:10:16 +0100

    fix: api tests sleep

--- tests/test_api_dev.py
+++ tests/test_api_dev.py
@@ -26,11 +26,11 @@ def image_id(lb_url_dev, test_image_path):
                 raise e
             connection_try += 1
             time.sleep(20)
-    time.sleep(10)
+    time.sleep(20)
     with open(test_image_path, "rb") as f:
         files = {"file": f}
         response = requests.post(f"http://{lb_url_dev}/image", files=files)
-
+    time.sleep(20)
     assert response.status_code == 200
     assert response.text.startswith("Image uploaded: ID: ")
 
